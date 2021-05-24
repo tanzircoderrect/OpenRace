04eb01666edcff7b766f67329aee9109777ad4ec
Addison Crump <addison.crump@coderrect.com>
2021-05-24 18:19:05 -0500

    Duplicate branches in Builder.h which could be bugs

--- src/IR/Builder.cpp
+++ src/IR/Builder.cpp
@@ -108,7 +108,7 @@ FunctionSummary race::generateFunctionSummary(const llvm::Function &func) {
         } else if (PthreadModel::isPthreadSpinLock(funcName)) {
           instructions.push_back(std::make_shared<PthreadSpinLock>(callInst));
         } else if (PthreadModel::isPthreadSpinUnlock(funcName)) {
-          instructions.push_back(std::make_shared<PthreadSpinLock>(callInst));
+          instructions.push_back(std::make_shared<PthreadSpinUnlock>(callInst));
         } else if (OpenMPModel::isForStaticInit(funcName)) {
           instructions.push_back(std::make_shared<OmpForInit>(callInst));
         } else if (OpenMPModel::isForStaticFini(funcName)) {
@@ -119,9 +119,7 @@ FunctionSummary race::generateFunctionSummary(const llvm::Function &func) {
           instructions.push_back(std::make_shared<OpenMPSingleEnd>(callInst));
         } else if (OpenMPModel::isBarrier(funcName)) {
           instructions.push_back(std::make_shared<OpenMPBarrier>(callInst));
-        } else if (OpenMPModel::isReduceStart(funcName)) {
-          instructions.push_back(std::make_shared<OpenMPReduce>(callInst));
-        } else if (OpenMPModel::isReduceNowaitStart(funcName)) {
+        } else if (OpenMPModel::isReduceStart(funcName) || OpenMPModel::isReduceNowaitStart(funcName)) {
           instructions.push_back(std::make_shared<OpenMPReduce>(callInst));
         } else if (OpenMPModel::isCriticalStart(funcName)){
           instructions.push_back(std::make_shared<OpenMPCriticalStart>(callInst));
